8b1994047df1bb661142e43bf652e04e72c0f2c2
yousef emad <yemad0348@gmail.com>
2021-07-22 22:52:40 +0200

    db

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://127.0.0.1:27017/LMS',{
+mongoose.connect('mongodb+srv://LMSProject:a12b34c.@cluster0.73ugb.mongodb.net/LMSDataBase?retryWrites=true&w=majority',{
     useNewUrlParser : true,
     useCreateIndex : true,
     useFindAndModify : false,
+    ssl : true
 })
